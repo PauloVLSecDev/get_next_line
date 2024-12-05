a4bd2794c82d568a8fd6dea9f8ead2f0fd6adaaf
Paulo Vitor Lopes Meira <pvitor-l@student.42sp.org.br>
2024-12-05 18:51:24 -0300

    beravior strange, but is better before

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,37 @@
 /*   By: pvitor-l <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 18:38:05 by pvitor-l          #+#    #+#             */
-/*   Updated: 2024/12/05 16:17:27 by pvitor-l         ###   ########.fr       */
+/*   Updated: 2024/12/05 18:37:51 by pvitor-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
+
+char	*read_file(int fd, char *buffer)
+{
+	char	*line;
+	int	size_read;
+
+	size_read = 1;
+	buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char *));
+	line = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char *));
+	if(!buffer)
+		return (NULL);
+	while(read_file > 0)
+	{
+		size_read = read(fd, line, BUFFER_SIZE + 1);
+		if (size_read == -1)
+		{
+			free(line);
+			return (NULL);
+		}
+		if(ft_strchr(line, '\n'))
+			break;
+	}
+	line[size_read] = '\0';
+	return (line);
+}
 
 char	*get_next_line(int fd)
 {
@@ -22,38 +48,13 @@ char	*get_next_line(int fd)
 	buffer = read_file(fd, buffer);
 	if(!buffer)
 		return (NULL);
-}
-
-char	*read_file(int fd, char *buffer)
-{
-	char	*line;
-	int	size_read;
-
-	size_read = 1;
-	buffer = (char *)malloc(BUFFER_SIZE * sizeof(char *));
-	line = (char *)malloc(BUFFER_SIZE * sizeof(char *));
-	if(!buffer)
-		return (NULL);
-	while(read_file > 0)
-	{
-		size_read = read(fd, line, BUFFER_SIZE);
-		if (size_read == -1)
-		{
-			free(line);
-			return (NULL);
-		}
-		line[size_read] = '\0';
-		if(ft_strchr(line, '\n'))
-			break;
-	}
-	line[size_read] = '\0';
-	return (line);
+	return(buffer);
 }
 
 int	main(void)
 {
 	int fd = open("read_me.txt", O_RDONLY);
 	char *line = get_next_line(fd);
-	printf("quantidade de linhas %s", line);
+	printf("%s", line);
 	
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: pvitor-l <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 18:38:58 by pvitor-l          #+#    #+#             */
-/*   Updated: 2024/11/24 18:43:27 by pvitor-l         ###   ########.fr       */
+/*   Updated: 2024/12/05 18:48:19 by pvitor-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 #define GET_NEXT_LINE_H
 
-# ifndef  BUFFER_SIZE
+#ifndef  BUFFER_SIZE
 #define  BUFFER_SIZE 42
 
 #include <stdlib.h>
