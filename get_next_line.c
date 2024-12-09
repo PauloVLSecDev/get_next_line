9f19dbf4154c6adbf6725ce9bd4ef15518ea9164
Paulo Vitor Lopes Meira <pvitor-l@student.42sp.org.br>
2024-12-09 16:41:06 -0300

    aaaaaaaaaaaa

--- get_next_line.c
+++ get_next_line.c
@@ -6,82 +6,70 @@
 /*   By: pvitor-l <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 18:38:05 by pvitor-l          #+#    #+#             */
-/*   Updated: 2024/12/08 19:23:37 by pvitor-l         ###   ########.fr       */
+/*   Updated: 2024/12/09 16:40:23 by pvitor-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-/*
-char	*ft_freeall(char *line, char *buffer)
+char	*extract_line(char **buffer)
 {
-	char	*temp;
+	char	*new_line;
 
-	temp = ft_strjoin(line, buffer);
-	free(buffer);
-	return (temp);
+	new_line = ft_strchr(*buffer, '\n');
+	
+	return (line)
 }
-*/
 
-char	*read_file(int fd, char *remaining)
+char	*read_file(int fd, char **buffer)
 {
-	char	*line;
-	int	size_read;
+	char	line[BUFFER_SIZE + 1];
+       	int	size_read;
+       	char	*temp; 
 
-	if(!remaining)
-		remaining = (char *)malloc(BUFFER_SIZE);
-	line = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
 	size_read = 1;
-	if(!line)
-	{
-		free(line);
-		return(NULL);
-	}
 	while(size_read > 0)
 	{
-		size_read = read(fd, line, BUFFER_SIZE + 1);
-		if (size_read == -1)
+		size_read = read(fd, line, BUFFER_SIZE);
+		if(size_read == -1)
 		{
 			free(line);
 			return (NULL);
 		}
+		line[BUFFER_SIZE] = '\0';
+		temp = ft_strjoin(*buffer, line);
+		free(buffer);
+		*buffer = temp;
 		if(ft_strchr(line, '\n'))
 			break;
 	}
-//	remaining = ft_freeall(line, remaining);
-	line[size_read] = '\0';
-	return (line);
+	return (buffer);
 }
 
 char	*get_next_line(int fd)
 {
 	static char	*buffer;
 
-	if(fd < 0 || BUFFER_SIZE <= 0)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	buffer = read_file(fd, buffer);
-//	line = read_line(&buffer);
+	read_file(fd, &buffer);
 	if(!buffer)
 		return (NULL);
-	return(buffer);
+	return (extract_line(&buffer));
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char *argv[])
 {
-    char *line;
-    if (argc < 2)
-    {
-        printf("Usage: %s <file>\n", argv[0]);
-        return 1;
-    }
-    int fd = open(argv[1], O_RDONLY);
-    while ((line = get_next_line(fd)) != NULL)
-    {
-        printf("%s", line);
-        free(line);
-    }
-    close(fd);
-    return 0;
-}
+	char	*line; 
+	int	fd;
 
+	if (argc < 2)
+		return (printf("without fd"));
+	fd = open(argv[1], O_RDONLY);
+	while ((line = get_next_line(fd))!= NULL) 
+	{
+		printf("%s", line);
+		free(line);
+	}
+}
